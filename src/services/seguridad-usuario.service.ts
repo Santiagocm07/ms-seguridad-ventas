7cf7ad8dfaf4c2310595c105169e31d49869f2b3
Santiagocm07 <santicm50@gmail.com>
2025-03-06 12:58:47 -0500

    Se hace la creación de usuario con la clave aleatoria y crifrada

---
+++ src/services/seguridad-usuario.service.ts
@@ -0,0 +1,51 @@
+import { /* inject, */ BindingScope, injectable} from '@loopback/core';
+import {repository} from '@loopback/repository';
+import {Credenciales, Usuario} from '../models';
+import {UsuarioRepository} from '../repositories';
+const generator = require('generate-password');
+const MD5 = require("crypto-js/md5");
+
+@injectable({scope: BindingScope.TRANSIENT})
+export class SeguridadUsuarioService {
+  constructor(
+    @repository(UsuarioRepository)
+    public repositorioUsuario: UsuarioRepository
+  ) { }
+
+  /**
+   * Crear una clave aleatoria
+   * @returns cadena aleatoria de n caracteres
+   */
+  crearTextoAleatorio(n: number): string {
+    let clave = generator.generate({
+      length: n,
+      numbers: true
+    });
+    return clave;
+  }
+
+  /**
+   * Cifrar una cadena con método MD5
+   * @param cadena texto a cifrar
+   * @returns cadena cifrada con MD5
+   */
+  cifrarTexto(cadena: string): string {
+    let cadenaCifrada = MD5(cadena).toString();
+    return cadenaCifrada;
+  }
+
+  /**
+   * Se busca un usuario por sus credenciales de acceso
+   * @param credenciales credenciales del usuario
+   * @returns usuario encontrado o null
+   */
+  async identificarUsuario(credenciales: Credenciales): Promise<Usuario | null> {
+    let usuario = await this.repositorioUsuario.findOne({
+      where: {
+        correo: credenciales.correo,
+        clave: credenciales.clave
+      }
+    });
+    return usuario as Usuario;
+  }
+}
