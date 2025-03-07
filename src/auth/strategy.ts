b8f970833184aaa55bf5fb30d738aa8458bf1a79
Santiagocm07 <santicm50@gmail.com>
2025-03-07 08:34:18 -0500

    Se crea una nueva carpeta llamada auth con un archivo strategy, en donde se realiza la estrategia de autorización lb4

---
+++ src/auth/strategy.ts
@@ -0,0 +1,16 @@
+import {AuthenticationStrategy} from '@loopback/authentication';
+import {Request} from '@loopback/rest';
+import {UserProfile} from '@loopback/security';
+
+export class AuthStrategy implements AuthenticationStrategy {
+  name: string = 'auth';
+
+  constructor(
+  ) { }
+
+  async authenticate(request: Request): Promise<UserProfile | undefined> {
+    console.log("Ejecutando estrategia");
+    return undefined;
+  }
+
+}
