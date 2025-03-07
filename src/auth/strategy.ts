8b7b6598b1103cfa6d807d6438c8bed94c7e9d68
Santiagocm07 <santicm50@gmail.com>
2025-03-07 11:39:02 -0500

    Realizar pruebas y ajustes a la estrategia de autorización, se soluciona el error con el metadata que siempre es un arreglo y le hacian falta las llaves del arreglo

--- src/auth/strategy.ts
+++ src/auth/strategy.ts
@@ -14,7 +14,7 @@ export class AuthStrategy implements AuthenticationStrategy {
     @service(SeguridadUsuarioService)
     private servicioSeguridad: SeguridadUsuarioService,
     @inject(AuthenticationBindings.METADATA)
-    private metadata: AuthenticationMetadata,
+    private metadata: AuthenticationMetadata[],
     @repository(RolMenuRepository)
     private repositorioRolMenu: RolMenuRepository
   ) { }
@@ -28,8 +28,9 @@ export class AuthStrategy implements AuthenticationStrategy {
     let token = parseBearerToken(request);
     if (token) {
       let idRol = this.servicioSeguridad.obtenerRolDesdeToken(token);
-      let idMenu: string = this.metadata.options![0];
-      let accion: string = this.metadata.options![1];
+      let idMenu: string = this.metadata[0].options![0];
+      let accion: string = this.metadata[0].options![1];
+      console.log(this.metadata);
 
       let permiso = await this.repositorioRolMenu.findOne({
         where: {
@@ -60,7 +61,7 @@ export class AuthStrategy implements AuthenticationStrategy {
         }
         if (continuar) {
           let perfil: UserProfile = Object.assign({
-            permiso: "OK"
+            permitido: "OK"
           });
           return perfil;
         } else {
