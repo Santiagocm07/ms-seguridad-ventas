3e6aaebc099996b9800d1ea3d19467d162119717
Santiagocm07 <santicm50@gmail.com>
2025-03-07 11:02:10 -0500

    Se hace la autenticación de un usuario frente a una acción en la base de datos. Consulta de permisos de un rol en la estrategia lb4 para obtener del token que llega, ya que con dicho token se podra acceder al rol especifico

--- src/config/seguridad.config.ts
+++ src/config/seguridad.config.ts
@@ -1,3 +1,9 @@
 export namespace ConfiguracionSeguridad {
   export const claveJWT: string = "Admin@2025*";
+  export const menuUsuarioId = "67c88cffa19e421300923c32";
+  export const listarAccion = "listar";
+  export const guardarAccion = "guardar";
+  export const editarAccion = "editar";
+  export const eliminarAccion = "eliminar";
+  export const descargarAccion = "descargar";
 }
